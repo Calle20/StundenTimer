c4a3ddfe4cf99da17268a4443959469e1b921d40
Calle20 <97348165+Calle20@users.noreply.github.com>
2022-01-09 08:26:56 +0100

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace StundenTimerNeu
+
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl_Clock = new System.Windows.Forms.Label();
+            this.Clock = new System.Windows.Forms.Timer(this.components);
+            this.groupBoxClock = new System.Windows.Forms.GroupBox();
+            this.lbl_Next = new System.Windows.Forms.Label();
+            this.groupBoxClock.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_Clock
+            // 
+            this.lbl_Clock.AutoSize = true;
+            this.lbl_Clock.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Clock.Location = new System.Drawing.Point(6, 26);
+            this.lbl_Clock.Name = "lbl_Clock";
+            this.lbl_Clock.Size = new System.Drawing.Size(96, 26);
+            this.lbl_Clock.TabIndex = 1;
+            this.lbl_Clock.Text = "24:24:24";
+            // 
+            // Clock
+            // 
+            this.Clock.Interval = 1000;
+            this.Clock.Tick += new System.EventHandler(this.Clock_Tick);
+            // 
+            // groupBoxClock
+            // 
+            this.groupBoxClock.Controls.Add(this.lbl_Clock);
+            this.groupBoxClock.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxClock.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxClock.Name = "groupBoxClock";
+            this.groupBoxClock.Size = new System.Drawing.Size(108, 67);
+            this.groupBoxClock.TabIndex = 2;
+            this.groupBoxClock.TabStop = false;
+            this.groupBoxClock.Text = "Uhrzeit";
+            // 
+            // lbl_Next
+            // 
+            this.lbl_Next.AutoSize = true;
+            this.lbl_Next.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Next.Location = new System.Drawing.Point(126, 27);
+            this.lbl_Next.Name = "lbl_Next";
+            this.lbl_Next.Size = new System.Drawing.Size(196, 52);
+            this.lbl_Next.TabIndex = 3;
+            this.lbl_Next.Text = "Um 7:40 Uhr klingelt\r\n     es zur Stunde";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 98);
+            this.Controls.Add(this.lbl_Next);
+            this.Controls.Add(this.groupBoxClock);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxClock.ResumeLayout(false);
+            this.groupBoxClock.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_Clock;
+        private System.Windows.Forms.Timer Clock;
+        private System.Windows.Forms.GroupBox groupBoxClock;
+        private System.Windows.Forms.Label lbl_Next;
+    }
+}
+
+
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace StundenTimerNeu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Clock_Tick(object sender, EventArgs e)
+        {
+            lbl_Clock.Text = DateTime.Now.ToString("hh:mm:ss");
+            if (DateTime.Now.ToString("HH:mm:ss") == "07:40:00")
+            {
+                Play_Stunde();
+                lbl_Next.Text = "Um 8:25 Uhr klingelt" + Environment.NewLine + " es zur Pause";
+
+            }
+            if (DateTime.Now.ToString("HH:mm:ss") == "08:25:00")
+            {
+                Play_Pause();
+                lbl_Next.Text = "Um 8:35 Uhr klingelt" + Environment.NewLine + " es zur Stunde";
+            }
+            if (DateTime.Now.ToString("HH:mm:ss") == "08:35:00")
+            {
+                Play_Stunde();
+                lbl_Next.Text = "Um 9:20 Uhr klingelt" + Environment.NewLine + " es zur Pause";
+            }
+            if (DateTime.Now.ToString("HH:mm:ss") == "09:20:00")
+            {
+                Play_Pause();
+                lbl_Next.Text = "Um 9:40 Uhr klingelt" + Environment.NewLine + " es zur Stunde";
+            }
+            if (DateTime.Now.ToString("HH:mm:ss") == "09:40:00")
+            {
+                Play_Stunde();
+                lbl_Next.Text = "Um 11:10 Uhr klingelt" + Environment.NewLine + " es zur Pause";
+            }
+            if (DateTime.Now.ToString("HH:mm:ss") == "11:10:00")
+            {
+                Play_Pause();
+                lbl_Next.Text = "Um 11:20 Uhr klingelt" + Environment.NewLine + " es zur Stunde";
+            }
+            if (DateTime.Now.ToString("HH:mm:ss") == "11:20:00")
+            {
+                Play_Stunde();
+                lbl_Next.Text = "Um 12:05 Uhr klingelt" + Environment.NewLine + " es zur Pause";
+            }
+            if (DateTime.Now.ToString("HH:mm:ss") == "12:05:00")
+            {
+                Play_Pause();
+                lbl_Next.Text = "Um 12:35 Uhr klingelt" + Environment.NewLine + " es zur Stunde";
+            }
+            if (DateTime.Now.ToString("HH:mm:ss") == "12:35:00")
+            {
+                Play_Stunde();
+                lbl_Next.Text = "Um 13:20 Uhr ist die " + Environment.NewLine + " Schule vorbei";
+            }
+            if (DateTime.Now.ToString("HH:mm:ss") == "13:20:00")
+            {
+                Play_Pause();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbl_Clock.Text = DateTime.Now.ToString("HH:mm:ss");
+            Clock.Start();
+        }
+        private static void Play_Pause()
+        {
+            SoundPlayer audio = new SoundPlayer(Properties.Resources.Pause);
+            audio.Play();
+        }
+        private static void Play_Stunde()
+        {
+            SoundPlayer audio = new SoundPlayer(Properties.Resources.Stunde);
+            audio.Play();
+        }
+    }
+}
